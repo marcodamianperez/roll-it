3d33f4cbc8f6d2937bf0f2aaefd5bc1b49f06f56
Marco Pérez <marcodamianperez@gmail.com>
2023-06-10 18:27:34 -0300

    update package.json and .gitignore

--- app.js
+++ app.js
@@ -11,8 +11,10 @@ const render = (e, use2Dice) => {
         
         const imageDice1 = document.querySelector('#image-dice1');
         const imageDice2 = document.querySelector('#image-dice2');
-        imageDice1.setAttribute('src', `./img/${number1}.svg`);
-        imageDice2.setAttribute('src', `./img/${number2}.svg`);
+        imageDice1.setAttribute('src', `./${number1}.svg`);
+        imageDice1.setAttribute('alt', `dice number ${number1}`);
+        imageDice2.setAttribute('src', `./${number2}.svg`);
+        imageDice2.setAttribute('alt', `dice number ${number2}`);
         imageDice2.style.display = 'inline';
         
         const total = document.querySelector('#total');
@@ -23,7 +25,8 @@ const render = (e, use2Dice) => {
         const number = generateRandomNumber();
 
         const imageDice1 = document.querySelector('#image-dice1');
-        imageDice1.setAttribute('src', `./img/${number}.svg`);
+        imageDice1.setAttribute('src', `./${number}.svg`);
+        imageDice1.setAttribute('alt', `dice number ${number}`);
         const total = document.querySelector('#total');
         total.textContent = number;
     }
