ef7fe4cc9055a10b1e9b6c68a03c163b3f757f2e
Marco Pérez <marcodamianperez@gmail.com>
2023-06-10 21:37:57 -0300

    update folder structure, package.json and .gitignore

--- src/app.js
+++ src/app.js
@@ -11,9 +11,9 @@ const render = (e, use2Dice) => {
         
         const imageDice1 = document.querySelector('#image-dice1');
         const imageDice2 = document.querySelector('#image-dice2');
-        imageDice1.setAttribute('src', `./${number1}.svg`);
+        imageDice1.setAttribute('src', `./img/${number1}.svg`);
         imageDice1.setAttribute('alt', `dice number ${number1}`);
-        imageDice2.setAttribute('src', `./${number2}.svg`);
+        imageDice2.setAttribute('src', `./img/${number2}.svg`);
         imageDice2.setAttribute('alt', `dice number ${number2}`);
         imageDice2.style.display = 'inline';
         
@@ -25,7 +25,7 @@ const render = (e, use2Dice) => {
         const number = generateRandomNumber();
 
         const imageDice1 = document.querySelector('#image-dice1');
-        imageDice1.setAttribute('src', `./${number}.svg`);
+        imageDice1.setAttribute('src', `./img/${number}.svg`);
         imageDice1.setAttribute('alt', `dice number ${number}`);
         const total = document.querySelector('#total');
         total.textContent = number;
